640c27952ad7d951c3e60a0092517eac38309069
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-08-11 08:44:52 +0200

    Update CheckBaterryToREDUsbE.py

--- CheckBaterryToREDUsbE.py
+++ CheckBaterryToREDUsbE.py
@@ -2,8 +2,9 @@ import psutil
 import os
 import socket
 
-
 MenoZariadenia = socket.gethostname()
+DlzkaMena = socket.gethostname()
+DlzkaMena = len(DlzkaMena)
 file_path = os.path.join("E:/AllCaptured/BatteryCheckPCorNotebook", "CheckIfIsPC.txt")
 
 def is_notebook_with_battery():
@@ -14,8 +15,12 @@ if __name__ == "__main__":
     if is_notebook_with_battery():
         print("Skript sa spustil na NOTEBOOKU s menom : " + MenoZariadenia)
         with open(file_path, "a") as file:
-            file.write("Skript sa spustil na notebooku.")
+            file.write("+"," ", "-"*25, " ", "+\n")
+            file.write("Skript sa spustil na notebooku.\n")
+            file.write("+"," ", "-"*25, " ", "+\n\n")
     else:
         print("Skript sa spustil na počítači.")
         with open(file_path, "a") as file:
-            file.write("Skript sa spustil na POCITACI s menom : " + MenoZariadenia)
+            file.write(" "*38 + "+" + " " + "-"*DlzkaMena + " " + "+\n")
+            file.write("Skript sa spustil na POCITACI s menom : " + MenoZariadenia + "\n")
+            file.write(" "*38 + "+" + " " + "-"*DlzkaMena + " " + "+\n\n")
